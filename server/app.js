c2957291d4db4a30fc1fa427fb179cd0c30c6001
rtuttle <rtuttle@1nn0v8.io>
2015-03-04 13:52:11 -0600

    Update to proper Node module scope conventions

--- server/app.js
+++ server/app.js
@@ -1,17 +1,20 @@
+// var io = require("socket.io")();
+
 var log = require("./log.js");
 
-var app = 
-{
-	// io: null
-};
-
-app.init = function init(httpServer)
+function init(httpServer)
 {
 	// TODO: add WebSocket (socket.io) endpoints or other light non-HTTP backend connections here as necessary (or move to a real app framework for Node like Express)
-	// app.io = require("socket.io")(httpServer);
-	// ...
+	// io.attach(httpServer);
 
 	log("Web application initialized");
 };
 
-module.exports.init = app.init;
+/*
+io.on("connection", function (socket)
+{
+	// ...
+});
+*/
+
+module.exports.init = init;
